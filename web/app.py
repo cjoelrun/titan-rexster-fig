52a0e9feffaba5bdaa879ad77d909ca9528f7484
cjoelrun <cameronjlopez@gmail.com>
2015-07-11 13:47:53 -0500

    add web

---
+++ web/app.py
@@ -0,0 +1,32 @@
+# people.py
+from bulbs.config import Config, DEBUG
+from bulbs.model import Node, Relationship
+from bulbs.property import String, Integer, DateTime
+from bulbs.utils import current_datetime
+
+
+class Person(Node):
+
+    element_type = "person"
+
+    name = String(nullable=False)
+    age = Integer()
+
+
+class Knows(Relationship):
+
+    label = "knows"
+
+    created = DateTime(default=current_datetime, nullable=False)
+
+from bulbs.rexster import Graph
+
+config = Config("titan:31182")
+config.set_logger(DEBUG)
+g = Graph(config)
+g.add_proxy("people", Person)
+g.add_proxy("knows", Knows)
+
+james = g.people.create(name="James")
+julie = g.people.create(name="Julie")
+g.knows.create(james, julie)
